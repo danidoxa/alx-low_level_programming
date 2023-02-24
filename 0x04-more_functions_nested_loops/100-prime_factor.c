beef53eaf8cd76e9959fc1aeac0e30016825b00b
danidoxa <daniel4real307@yahoo.com>
2023-02-23 16:22:32 -0800

    More functions, more nested loops

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -13,16 +13,28 @@ int main(void)
 	long int i;
 
 	n = 612852475143;
-max = -1;
-while (n % 2 == 0)
-max = 2;
-n /= 2;
-for (i = 3; i <= sqrt(n); i = i + 2)
-while (n % i == 0)
-max = i;
-n = n / i;
-if (n > 2)
-max = n;
-printf("%ld\n", max);
-return (0);
+	max = -1;
+
+	while (n % 2 == 0)
+	{
+		max = 2;
+		n /= 2;
+	}
+
+	for (i = 3; i <= sqrt(n); i = i + 2)
+	{
+		while (n % i == 0)
+		{
+			max = i;
+			n = n / i;
+		}
+	}
+
+	if (n > 2)
+		max = n;
+
+	printf("%ld\n", max);
+
+	return (0);
 }
+
--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,11 +7,20 @@
 void print_square(int size)
 {
 	if (size <= 0)
+	{
 		_putchar('\n');
-	else
+	} else
+	{
 		int i, j;
-	for (i = 0; i < size; i++)
-		for (j = 0; j < size; j++)
-			_putchar('#');
-	_putchar('\n');
+
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
 }
+
